e597e82959d9c7e8d36c583256ec495f1a18b6ca
Joel M. Montalvan <132353287+joel3m25@users.noreply.github.com>
2024-01-10 19:11:08 +0100

    Update Ex1VersioIterativa

--- src/main/kotlin/Ex1VersioIterativa.kt
+++ src/main/kotlin/Ex1VersioIterativa.kt
@@ -1,4 +1,9 @@
 import java.util.*
+
+/**
+ * Función principal que solicita al usuario dos números y muestra los resultados
+ * de diferentes funciones aplicadas al primer número.
+ */
 fun main() {
     val sc = Scanner(System.`in`)
     println("Introduce el primer número: ")
@@ -11,7 +16,13 @@ fun main() {
     println(m3(a))
     println(m4(a))
 }
-
+/**
+ * @author Joel Montalvan y Daniel Reinosa
+ * Función que calcula el producto de los números del 1 al n.
+ *
+ * @param n Número hasta el cual se realiza el producto.
+ * @return El producto de los números del 1 al n.
+ */
 fun m1(n: Int): Int {
     var result = 0
     for (i in 1..n) {
@@ -19,7 +30,14 @@ fun m1(n: Int): Int {
     }
     return result
 }
-
+/**
+ * @author Joel Montalvan y Daniel Reinosa
+ * Función que suma los números del 1 al n y devuelve el resultado como un entero.
+ *
+ * @param x Número de punto flotante que se ignora en esta función.
+ * @param n Número hasta el cual se realiza la suma.
+ * @return La suma de los números del 1 al n.
+ */
 fun m2(x: Double, n: Int): Int {
     var result = 0
     for (i in 1..n) {
@@ -27,7 +45,13 @@ fun m2(x: Double, n: Int): Int {
     }
     return result
 }
-
+/**
+ * @author Joel Montalvan y Daniel Reinosa
+ * Función que cuenta la cantidad de veces que se ejecuta el bucle del 1 al n.
+ *
+ * @param n Número hasta el cual se cuenta.
+ * @return La cantidad de veces que se ejecuta el bucle del 1 al n.
+ */
 fun m3(n: Int): Int {
     var result = 0
     for (i in 1..n) {
@@ -36,6 +60,13 @@ fun m3(n: Int): Int {
     return result
 }
 
+/**
+ * @author Joel Montalvan y Daniel Reinosa
+ * Función que suma los números del 1 al n y devuelve el resultado.
+ *
+ * @param n Número hasta el cual se realiza la suma.
+ * @return La suma de los números del 1 al n.
+ */
 fun m4(n: Int): Int {
     var result = 0
     for (i in 1..n) {
