c861932555d71586dddb34968ef0f7bba772351e
Joel M. Montalvan <132353287+joel3m25@users.noreply.github.com>
2024-01-10 19:06:36 +0100

    Update Ex3VidaMart

--- src/main/kotlin/Ex3VidaMart.kt
+++ src/main/kotlin/Ex3VidaMart.kt
@@ -1,4 +1,8 @@
 import java.util.Scanner
+
+/**
+ * Función principal que solicita al usuario dos números y muestra el resultado de su multiplicación.
+ */
 fun main() {
         val sc = Scanner(System.`in`)
     println("Introduce el primer número: ")
@@ -9,6 +13,14 @@ fun main() {
     println(multiplicar(5, 5))
 }
 
+/**
+ * @author Joel Montalvan y Daniel Reinosa
+ * Función que realiza la multiplicación de dos números enteros.
+ *
+ * @param a Primer número a multiplicar.
+ * @param b Segundo número a multiplicar.
+ * @return El resultado de la multiplicación.
+ */
 fun multiplicar(a: Int, b: Int): Int {
     var result = 0
     for (i in 1..b) {
